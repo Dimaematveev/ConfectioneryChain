ce76b9fdf50028bf6528a9324b8b26ab6abe1c28
dimaematveev <dimaematveev@gmail.com>
2020-10-19 00:35:37 +0300

    Создал Class1 для проверки что от него можно унаследоваться

--- ConfectioneryChain.DB/ParticalClass/BuyGood.cs
+++ ConfectioneryChain.DB/ParticalClass/BuyGood.cs
@@ -1,6 +1,8 @@
-﻿namespace ConfectioneryChain.DB
+﻿using ConfectioneryChain.DB.ParticalClass;
+
+namespace ConfectioneryChain.DB
 {
-    public partial class BuyGood
+    public partial class BuyGood : Class1
     {
         public override string ToString()
         {
